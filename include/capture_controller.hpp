ee4763bde4aa4b07724af592cb4cbc6afc2d9a95
Finn McConville <finnmcco03@gmail.com>
2026-05-04 17:56:43 +0100

    Remove outdated files

--- include/capture_controller.hpp
+++ include/capture_controller.hpp
@@ -2,6 +2,7 @@
 #include <libcamera/libcamera.h>
 #include "camera.hpp"
 #include "camera_queue.hpp"
+#include "ir_queue.hpp"
 #include <thread>
 #include <chrono>
 
@@ -9,7 +10,9 @@ class CaptureController {
 public:
     CaptureController()
         :   camera_manager_(std::make_unique<libcamera::CameraManager>()),
-            queue_(300),
+            cam_queue_(300),
+            ir_queue_(50),
+            ir_capture_(ir_queue_, "/dev/i2c-1"),
             cam0_(camera_manager_.get(), 0, queue_),
             cam1_(camera_manager_.get(), 1, queue_)
     {
@@ -19,6 +22,7 @@ public:
     void start_capture(){
         cam0_.start();
         cam1_.start();
+        ir_capture_.start();
         
         //wait for the cameras to wake up fully
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
@@ -32,7 +36,9 @@ public:
     void stop_capture() {
         cam0_.stop();
         cam1_.stop();
-        queue_.stop();
+        ir_capture_.stop();
+        cam_queue_.stop();
+        ir_queue_.stop();
         
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
         camera_manager_->stop();
@@ -42,18 +48,29 @@ public:
         }
     }
     
-    CameraQueue& get_queue() {
-        return queue_;
+    CameraQueue& get_cam_queue() {
+        return cam_queue_;
+    }
+
+    IRQueue& get_ir_queue() {
+        return ir_queue_;
     }
     
-    std::size_t queue_size() {
-        return queue_.size();
+    std::size_t cam_queue_size() {
+        return cam_queue_.size();
+    }
+
+    std::size_t ir_queue_size() {
+        return ir_queue_.size();
     }
 
 private:
     std::thread timer_thread_;
     std::unique_ptr<libcamera::CameraManager> camera_manager_;
-    CameraQueue queue_;
+    CameraQueue cam_queue_;
+    IRQueue ir_queue_;
+    IRCapture ir_capture_;
+
     Camera cam0_;
     Camera cam1_;
 };
