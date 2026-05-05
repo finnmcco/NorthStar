8c8a2015d23c58690c833deafb93644319dc9473
finnmcco <finnmcco03@gmail.com>
2026-05-05 14:11:02 +0100

    commit removal

--- include/frame_packet.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-#include <vector>
-
-struct FramePacket {
-    uint8_t              camera_id  = 0;
-    uint64_t             timestamp  = 0;  // nanoseconds
-    std::vector<uint8_t> data;
-};
